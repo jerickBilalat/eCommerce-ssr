f87f5dd63affff6c8cd424227feb4a6ce9b3093f
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-07-16 18:11:43 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const config = {
+  apikey: '021930de45114c51987d9eeee3b04e5c'
+};
+export default config;
---
+++ src/client/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="page-footer red">
+      <div className="footer-copyright">
+        <div className="container">
+          © 2019 SSR Daily News - ilker ALTIN
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/ilkeraltin/react-ssr-news"
+          >
+            Source Code
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
