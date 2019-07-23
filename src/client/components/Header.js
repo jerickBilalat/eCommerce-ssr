69f299c0692f628b146778f8db993f604527a246
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-07-23 18:53:24 -0500

    fix remove menu trigger for more simple nav

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,16 +1,14 @@
-import React, {Fragment} from 'react';
-import { Link, withRouter } from "react-router-dom";
-import {connect} from 'react-redux';
+import React, { Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class Header extends React.Component{
-
+class Header extends React.Component {
   render() {
-    const navigationLinks = [{name: "Shop", path:"/"}];
+    const navigationLinks = [{ name: 'Shop', path: '/' }];
     return (
       <Fragment>
         <div className="row">
           <div className="header">
-
             <div className="col-md-3 col-sm-12">
               <div id="logo">
                 <h1>JRK POOLS</h1>
@@ -21,47 +19,48 @@ class Header extends React.Component{
         </div>
         <div className="row">
           <div className="container">
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="menu-responsive">
-                    <i
-                      className="fa fa-chevron-circle-right menu-trigger"
-                    />
-                  </div>
-                  <nav id="navigation">
-                    <ul className="menu" id="responsive">
-                      {navigationLinks.map(item => (
-                        <li key={item.name}>
-                          <Link
-                            className={(this.props.match.path === item.path && "current") || " "}
-                            to={item.path}
-                          >
-                            {item.name}
-                          </Link>
-                        </li>
-                      ))}
-                      <li className={(this.props.match.path === "/cart" && "current cart-icon") || "cart-icon"}>
-                        <Link to="/cart">
-                          <i className="fa fa-shopping-cart" />
-                          {this.props.cartItems ? <span className="cart-counter">{this.props.cartItems.length}</span> : null}
+            <div className="row">
+              <div className="col-md-12">
+                <nav id="navigation">
+                  <ul className="menu">
+                    {navigationLinks.map(item => (
+                      <li key={item.name}>
+                        <Link
+                          className={(this.props.match.path === item.path && 'current') || ' '}
+                          to={item.path}
+                        >
+                          {item.name}
                         </Link>
                       </li>
-                    </ul>
-                  </nav>
-                </div>
+                    ))}
+                    <li
+                      className={
+                        (this.props.match.path === '/cart' && 'current cart-icon') || 'cart-icon'
+                      }
+                    >
+                      <Link to="/cart">
+                        <i className="fa fa-shopping-cart" />
+                        {this.props.cartItems ? (
+                          <span className="cart-counter">{this.props.cartItems.length}</span>
+                        ) : null}
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
               </div>
             </div>
-            <div className="clearfix" />
+          </div>
+          <div className="clearfix" />
         </div>
       </Fragment>
-    )
+    );
   }
 }
 
 function mapStateToProps(store) {
   return {
     cartItems: store.cart.cartItems
-  }
+  };
 }
 
 export default withRouter(connect(mapStateToProps)(Header));
